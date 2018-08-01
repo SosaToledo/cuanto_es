c47e14e51d5a70c14def76bc2669b404f442b83c
Luciano <luchososa1990@hotmail.com.ar>
2018-08-01 12:55:00 -0300

    Función eliminar
    Este es el commit piola

--- app/src/main/java/com/luciano/thinco/cuanto_es/participantesAdapter.java
+++ app/src/main/java/com/luciano/thinco/cuanto_es/participantesAdapter.java
@@ -1,5 +1,6 @@
 package com.luciano.thinco.cuanto_es;
 
+import android.content.DialogInterface;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
@@ -9,7 +10,7 @@ import android.widget.TextView;
 
 import java.util.List;
 
-public class participantesAdapter extends RecyclerView.Adapter<participantesAdapter.participantesViewHolder> {
+public class participantesAdapter extends RecyclerView.Adapter<participantesAdapter.participantesViewHolder> implements itemClickListenerUnity {
     private List<participantes> participantesList;
 
     @NonNull
@@ -17,7 +18,7 @@ public class participantesAdapter extends RecyclerView.Adapter<participantesAdap
     public participantesViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
         View v = LayoutInflater.from(viewGroup.getContext())
                 .inflate(R.layout.participante_card, viewGroup, false);
-        return new participantesViewHolder(v);
+        return new participantesViewHolder(v, this);
     }
 
     @Override
@@ -31,15 +32,40 @@ public class participantesAdapter extends RecyclerView.Adapter<participantesAdap
         return participantesList.size();
     }
 
-    public static class participantesViewHolder extends RecyclerView.ViewHolder{
+
+    public static class participantesViewHolder extends RecyclerView.ViewHolder implements View.OnLongClickListener{
         public TextView nombre;
         public TextView monto;
+        public itemClickListenerUnity listener;
 
-        public participantesViewHolder(View V){
+        public participantesViewHolder(View V, itemClickListenerUnity listener){
             super (V);
-            nombre = (TextView) V.findViewById(R.id.tvnombre);
-            monto = (TextView) V.findViewById(R.id.tvmonto);
+            nombre = V.findViewById(R.id.tvnombre);
+            monto = V.findViewById(R.id.tvmonto);
+            this.listener = listener;
+            itemView.setOnLongClickListener(this);
 
+        }
+
+        @Override
+        public boolean onLongClick(final View V) {
+            final android.app.AlertDialog.Builder dialogConfirmacion = new android.app.AlertDialog.Builder(V.getContext());
+            dialogConfirmacion.setTitle("Eliminar a "+nombre.getText().toString());
+            dialogConfirmacion.setCancelable(true);
+            dialogConfirmacion.setPositiveButton("Eliminar", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    listener.itemLongClick(V, getAdapterPosition());
+                }
+            });
+            dialogConfirmacion.setNegativeButton("Cancelar", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+
+                }
+            });
+            dialogConfirmacion.show();
+            return true;
         }
     }
 
@@ -47,5 +73,13 @@ public class participantesAdapter extends RecyclerView.Adapter<participantesAdap
         this.participantesList = items;
     }
 
+    @Override
+    public void itemLongClick(View v, int position) {
+        participantesList.remove(position);
+        notifyDataSetChanged();
+    }
+}
 
+interface itemClickListenerUnity{
+        void itemLongClick(View v, int position);
 }
